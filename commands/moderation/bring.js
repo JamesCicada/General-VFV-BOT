2f6de61ecdfb1d48a137a58e41c1895c4b2e2733
James-Cicada <64677547+JamesCicada@users.noreply.github.com>
2022-06-27 17:22:07 +0100

    added bring command

---
+++ commands/moderation/bring.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("bring")
+        .setDescription("Bring someone to your voice channel")
+        .setDefaultMemberPermissions(2)
+        .addUserOption((option) =>
+            option
+                .setName("user")
+                .setDescription("the channel you wanna bring")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        try {
+            let targetMember = interaction.options.getMember("user");
+            let target = interaction.options.getUser("user");
+            let user = interaction.user;
+            let userMember = interaction.member;
+            let targetChannel = interaction.member.voice.channel;
+            if (!targetMember.voice.channel) {
+                await interaction.reply(
+                    `${targetMember} is not in a voice channel`
+                );
+                return;
+            } else if (!userMember.voice.channel) {
+                await interaction.reply(
+                    "you need to join a voice channel first"
+                );
+            } else {
+                await targetMember.voice.setChannel(targetChannel);
+                await interaction.reply(
+                    `i moved ${targetMember} to you in ${targetChannel}`
+                );
+            }
+
+            //console.log(member, newNick);
+        } catch (err) {
+            console.log(err);
+            interaction.reply({
+                content: "i don't have permission to do that",
+                ephemeral: true,
+            });
+        }
+    },
+};
+//DONE
