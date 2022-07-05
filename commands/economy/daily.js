30ab316122e9454a7e26a3f4a569a32915142a7e
James-Cicada <64677547+JamesCicada@users.noreply.github.com>
2022-07-05 16:57:27 +0100

    accurate timeleft for daily

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -5,7 +5,7 @@ const moment = require("moment");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("daily")
-        .setDescription("take your daily 𝒱 creds"),
+        .setDescription("claim your daily 𝒱 creds"),
     async execute(interaction) {
         let username = interaction.user.username;
         let userId = interaction.user.id;
@@ -16,14 +16,25 @@ module.exports = {
         let nowDate = moment(new Date().now).format();
         let expired = moment(date).add({ hours: 24 }).format();
         //let expiredNoFormat = moment(new Date().now).add({ minutes: 5 });
-        console.log(expired);
+        //console.log(expired);
         //'2013-03-24T10:15:20:12Z';
         let timeoutCheck = await memberSchema.findOne({ discordId: userId });
         let checkTimeout = await timeoutCheck.dailyCooldown;
-        console.log(checkTimeout);
-        console.log(expired);
-        console.log(moment(checkTimeout).isAfter(expired));
-        let timeLeft = await moment(checkTimeout).diff(nowDate, "hours");
+        //console.log(checkTimeout);
+        //console.log(expired);
+        //console.log(moment(checkTimeout).isAfter(expired));
+        //let timeLeft = await moment(checkTimeout).diff(nowDate, "hours", true);
+        let timeLeftMin = await moment(checkTimeout).diff(
+            nowDate,
+            "hours",
+            true
+        );
+        //console.log(timeLeftMin);
+        let minutes = timeLeftMin.toString().split(".").pop();
+        let minutesLast = `0.${minutes}` * 60;
+        let hours = Math.floor(timeLeftMin);
+        let leftMinutes = Math.round(minutesLast);
+        console.log(leftMinutes);
 
         try {
             if (moment(date).isSameOrAfter(checkTimeout) || !checkTimeout) {
@@ -37,7 +48,11 @@ module.exports = {
                 );
             } else {
                 interaction.reply(
-                    "`you still need to wait " + timeLeft + " hours `"
+                    "`you still need to wait " +
+                        hours +
+                        " hours " +
+                        leftMinutes +
+                        " minutes`"
                 );
             }
         } catch (err) {
